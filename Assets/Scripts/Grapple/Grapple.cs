fda03d508efd622382afaae4c4acb265f26a1d53
joshcheung21 <joshss212312@gmail.com>
2019-09-13 15:34:50 -0600

    Grapple Added

---
+++ Assets/Scripts/Grapple/Grapple.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    //Speed of pulling and moving towards objects
+    [Tooltip("Speed of pulling and moving towards objects")]
+    [Range(.2f, .8f)]
+    public float timePerGrappleMovement = .05f;
+
+    [Tooltip("Determines if player can shoot bad guys or not")]
+    public bool combatMode = false;
+    [Tooltip("Distance between player and object after pulled")]
+    public float distanceBetweenPulledObjs;
+    public GameObject grappleHook;
+    public GameObject target;
+    public GameObject parent;
+    bool isGrappled = false;
+    bool grappleFired = false;
+    bool pullingObject = false;
+    bool pullingToObject = false;
+    GrappleFinder grappleFinder;
+    GameObject grappledObj;
+    
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GameObject.Find("PlayerMaster");
+        grappleFinder = GetComponentInChildren<GrappleFinder>();
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        if (!isGrappled && !grappleFired && !pullingObject && !pullingToObject)
+        {
+            transform.position = parent.transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (!isGrappled && !grappleFired)
+            {
+                ShootGrapple();
+                //StartCoroutine(ShootGrappleProj());
+            }
+            else
+            {
+                isGrappled = false;
+                grappleFired = false;
+                grappleHook.transform.position = parent.transform.position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && !pullingObject && !pullingToObject)
+        {
+            if (isGrappled)
+            {
+                StartCoroutine(PullObjectTowards());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !pullingObject && !pullingToObject)
+        {
+            if (isGrappled)
+            {
+                StartCoroutine(PullToPosition());
+
+            }
+        }
+        //Debug.DrawRay(transform.position, transform.up * 1000, Color.red,100);
+
+
+    }
+
+
+    IEnumerator PullObjectTowards()
+    {
+        pullingObject = true;
+        while (true)
+        {
+            if (Mathf.Abs(Vector3.Distance(parent.transform.position, grappledObj.transform.position)) < distanceBetweenPulledObjs)
+            {
+                grappledObj = null;
+                isGrappled = false;
+                pullingObject = false;
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+            //Vector3 dir = grappledPos - (Vector3)parent.transform.position;
+
+
+            Vector3 dir = grappledObj.transform.position - parent.transform.position;
+            grappleHook.transform.position -= dir.normalized * timePerGrappleMovement;
+            grappledObj.transform.position -= dir.normalized * timePerGrappleMovement;
+        }
+        grappleFired = false;
+        // Resets grapple
+        grappleHook.transform.position = parent.transform.position;
+    }
+    IEnumerator PullToPosition()
+    {
+        pullingToObject = true;
+        Vector3 grapplePos = grappleHook.transform.position;
+        while (true)
+        {
+            grappleHook.transform.position = grapplePos;
+            if (Mathf.Abs(Vector3.Distance(parent.transform.position, grappledObj.transform.position)) < distanceBetweenPulledObjs)
+            {
+                grappledObj = null;
+                isGrappled = false;
+                pullingToObject = false;
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+            Vector3 dir = grappledObj.transform.position - parent.transform.position;
+
+            parent.transform.position += dir.normalized * timePerGrappleMovement;
+            //print(dir.normalized * timePerGrappleMovement);
+        }
+        //Resets Grapple
+        grappleFired = false;
+        grappleHook.transform.position = parent.transform.position;
+    }
+
+    void ShootGrapple()
+    {
+        if (grappleFinder.shortestObj == null)
+        {
+            return;
+        }
+        else
+        {
+            grappleFired = true;
+            grappledObj = grappleFinder.shortestObj;
+            grappleHook.transform.position = grappledObj.transform.position;
+            isGrappled = true;
+            if (combatMode)
+            {
+                //Do bad things to enemy here
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Grapple/GrappleFinder.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleFinder : MonoBehaviour
+{
+
+    public GameObject shortestObj;
+    public GameObject player;
+    Grapple grapple;
+    public 
+    float shortestDistance = Mathf.Infinity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerMaster");
+        grapple = GetComponentInParent<Grapple>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shortestObj = FindTarget();
+        print(shortestObj);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(Mathf.Abs(Vector3.Distance(player.transform.position,other.transform.position)) < shortestDistance)
+        {
+            shortestObj = other.gameObject;
+            shortestDistance = Mathf.Abs(Vector3.Distance(player.transform.position, other.transform.position));
+        }
+    }
+
+    GameObject FindTarget()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position + transform.forward * 10, 10);
+        float shortestDistance = Mathf.Infinity;
+        Collider shortestCollider = null;
+        foreach (Collider col in colliders)
+        {
+            if(col.tag == "CanGrapple" && !grapple.combatMode)
+            {
+                if (Vector3.Distance(player.transform.position, col.transform.position) < shortestDistance)
+                {
+                    shortestDistance = Vector3.Distance(player.transform.position, col.transform.position);
+                    shortestCollider = col;
+                }
+            }
+            if(col.tag == "Enemy" && grapple.combatMode)
+            {
+                if (Vector3.Distance(player.transform.position, col.transform.position) < shortestDistance)
+                {
+                    shortestDistance = Vector3.Distance(player.transform.position, col.transform.position);
+                    shortestCollider = col;
+                }
+            }
+            
+        }
+        if (shortestCollider == null)
+            return null;
+        return shortestCollider.gameObject;
+    }
+
+    private void OnDrawGizmos()
+    {
+       // Gizmos.color = Color.yellow;
+        //Gizmos.DrawSphere(transform.position + transform.forward * 15, 15);
+    }
+}
