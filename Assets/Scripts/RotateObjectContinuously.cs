19502b11dd071421cc621a7404c1c271fc2091c8
Chris <47432857+Chris@users.noreply.github.com>
2020-11-17 20:42:38 -0700

    Add fog plane rotation effect

---
+++ Assets/Scripts/RotateObjectContinuously.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectContinuously : MonoBehaviour
+{
+    [SerializeField] private float rotationRate = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Vector3 currentRot = this.gameObject.transform.rotation.eulerAngles;
+        //print(currentRot);
+        this.gameObject.transform.Rotate(new Vector3(0f, rotationRate, 0f));
+    }
+}
